d97fd0c14c067e4f08331018fd307457152029dc
Aaron Williams <aaronrw4@yahoo.com>
2021-09-20 16:15:57 -0700

    Created Banner.js

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner() {
+    return (
+        <header>
+            <h1>Scroll to Top Example Page</h1>
+        </header>
+    )
+}
+
+export default Banner
