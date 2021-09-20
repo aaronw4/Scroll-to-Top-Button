97c6299127915aefb96f233784990a90addc6bc7
Aaron Williams <aaronrw4@yahoo.com>
2021-09-20 16:16:42 -0700

    Created App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Banner from "./components/Banner";
+import Content from "./components/Content";
+import ScrollButton from "./components/scrollButton";
+
+function App() {
+    return (
+        <div>
+            <Banner/>
+            <Content/>
+            <ScrollButton/>
+        </div>
+    )
+}
+
+export default App;
